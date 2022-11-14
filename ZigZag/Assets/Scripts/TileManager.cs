004830c6d1e46b6a68287c4b355855b0386cbe4b
TanyaBanina <115241739+TanyaBanina@users.noreply.github.com>
2022-11-15 00:23:34 +0200

    Added falling tiles and optimized tile generation

--- ZigZag/Assets/Scripts/TileManager.cs
+++ ZigZag/Assets/Scripts/TileManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TileManager : MonoBehaviour
@@ -8,6 +9,8 @@ public class TileManager : MonoBehaviour
     public GameObject currentTile;
     public GameObject[] tilePrefabs;
     private static TileManager instance;
+    private Stack<GameObject> leftTiles = new Stack<GameObject>();
+    private Stack<GameObject> topTiles = new Stack<GameObject>();
 
     public static TileManager Instance { 
         get { 
@@ -16,12 +19,15 @@ public class TileManager : MonoBehaviour
         } 
     }
 
+    public Stack<GameObject> LeftTiles { get => leftTiles; set => leftTiles = value; }
+    public Stack<GameObject> TopTiles { get => topTiles; set => topTiles = value; }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("i was start");
-        for (int i = 0; i < 5; i++)
+        createTiles(30);
+        for (int i = 0; i < 20; i++)
         {
             spawnTile();
         }
@@ -32,11 +38,35 @@ public class TileManager : MonoBehaviour
     {
         
     }
+    public void createTiles(int amount)
+    {
+        for(int i = 0;i<amount; i++)
+        {
+            LeftTiles.Push(Instantiate(tilePrefabs[0]));
+            TopTiles.Push(Instantiate(tilePrefabs[1]));
+            LeftTiles.Peek().name = "LeftTile";
+            TopTiles.Peek().name = "TopTile";
+            TopTiles.Peek().SetActive(false);
+            LeftTiles.Peek().SetActive(false);
+        }
+    }
     public void spawnTile()
     {
-        Debug.Log("i was here");
+        //if(LeftTiles.Count == 0 || LeftTiles.Count == 0) { createTiles(10); }
+
         int randomIndex = Random.Range(0, 2);
-        currentTile = (GameObject)Instantiate(tilePrefabs[randomIndex],currentTile.transform.GetChild(0).position, 
-            Quaternion.identity);
+        if(randomIndex == 0)
+        {
+            GameObject tmp = LeftTiles.Pop();
+            tmp.SetActive(true);
+            tmp.transform.position = currentTile.transform.GetChild(0).position;
+            currentTile = tmp;
+        }
+        else if(randomIndex == 1){
+            GameObject tmp = TopTiles.Pop();
+            tmp.SetActive(true);
+            tmp.transform.position = currentTile.transform.GetChild(0).position;
+            currentTile = tmp;
+        }
     }
 }
--- ZigZag/Assets/Scripts/TileScript.cs
+++ ZigZag/Assets/Scripts/TileScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TileScript : MonoBehaviour
 {
+    float fallDelay = 0.8f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +19,27 @@ public class TileScript : MonoBehaviour
     void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player") TileManager.Instance.spawnTile();
+        StartCoroutine(fallDown());
+    }
+
+    IEnumerator fallDown()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        GetComponent<Rigidbody>().isKinematic= false;
+        yield return new WaitForSeconds(2);
+        switch (gameObject.name)
+        {
+            case "LeftTile":
+                TileManager.Instance.LeftTiles.Push(gameObject);
+                gameObject.GetComponent<Rigidbody>().isKinematic= true;
+                gameObject.SetActive(false);
+                break;
+
+            case "TopTile":
+                TileManager.Instance.TopTiles.Push(gameObject);
+                gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                gameObject.SetActive(false);
+                break;
+        }
     }
 }
